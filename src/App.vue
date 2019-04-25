10513e41a368cc7b8ac1aa65ff788d4760839d02
王宇 <wangyu02480@klicen.com>
2019-04-25 16:20:10 +0800

    テスト

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome ひめたん"/>
+    <HelloWorld msg="Welcome ひめたんんんんんんん"/>
   </div>
 </template>
 
