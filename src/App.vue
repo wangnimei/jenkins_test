d3a0cf8f9fcd626ac87f5f8daaabc5740e68c37f
王宇 <wangyu02480@klicen.com>
2019-07-17 16:11:08 +0800

    feat: 🎸 黑莓糖

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,26 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  mounted() {
+    function sleep(duration) {
+      return new Promise(function (resolve, reject) {
+        console.log("b");
+
+        setTimeout(resolve, 1000);
+
+        setTimeout(() => {
+          console.log('d');
+        }, 1000);
+      })
+    }
+    
+    console.log("a");
+    sleep().then(() => {
+      var begin = Date.now();
+      while(Date.now() - begin < 1000);
+      console.log("c");
+    });
   }
 }
 </script>
